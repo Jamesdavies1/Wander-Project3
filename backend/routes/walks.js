7e8992f94f7976a9f6b37b33a998225f9f013f74
james davies <jamesdavies@jamess-imac.home>
2020-04-08 22:56:14 +0100

    added new update and delete requests inside walk.js route file

--- backend/routes/walks.js
+++ backend/routes/walks.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 let Walks = require("../models/walks");
 
+//GET REQUEST
 router.route("/").get((req, res) => {
 Walks.find()
 .then(walks => res.json(walks))
 .catch(err => res.status(400).json("error: " + err));
 });
 
+//POST REQUEST
 router.route("/add").post((req, res) => {
     const name = req.body.name;
     const difficulty = req.body.difficulty;
@@ -24,5 +26,34 @@ router.route("/add").post((req, res) => {
     .then(() => res.json("New walk added!"))
     .catch(err => res.status(400).json("error: " + err));
 });
+
+//GET REQUEST
+router.route("/:id").get((req, res) => {
+    Walks.findById(req.params.id)
+    .then(walks => res.json(walks))
+    .catch(err => res.status(400).json("error: " + err));
+});
+
+//DELETE REQUEST
+router.route("/:id").delete((req, res) => {
+    Walks.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Walk deleted."))
+    .catch(err => res.status(400).json("error: " + err));
+});
+
+//UPDATE REQUEST
+router.route("/update/:id").post((req, res) => {
+    Walks.findById(req.params.id)
+    .then(walks => {
+        walks.name = req.body.name;
+        walks.difficulty = req.body.difficulty;
+        walks.time = Number(req.body.time);
+        walks.location = req.body.location;
+
+        walks.save()
+        .then(() => res.json("Walk updated!."))
+        .catch(err => escape.status(400).json("error: " + err));
+        });
+    });
 
 module.exports = router;
