d8a42fd65c334438f5d722938ff9e672cdc9c40b
james davies <jamesdavies@jamess-imac.home>
2020-04-09 21:37:54 +0100

    added new component dropdownform and implemented this into selectwalk page

--- client/src/pages/SelectWalk.js
+++ client/src/pages/SelectWalk.js
@@ -1,12 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import DropDownForm from "../components/DropDownForm";
 
 function SelectWalk() {
     return (
         <div>
+<DropDownForm contents={["easy", "medium", "hard"]} title={"Difficulty level"}/>
+<DropDownForm contents={[0.5, 1, 2, 4, 6, 8]} title={"Time to complete"}/>
+<DropDownForm contents={["Cheshire", "Lake District", "Peak District", "Scotland", "Snowdonia", "Yorkshire Dales"]} title={"Region of activity"}/>
 
         </div>
     )
 };
+
+
 
 export default SelectWalk;
