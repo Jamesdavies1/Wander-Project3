4d7641bcd400dc52bae10dfbc891dc016c92b569
james davies <jamesdavies@jamess-imac.home>
2020-04-10 10:25:12 +0100

    added home button component and used in select walk page

---
+++ client/src/components/HomeButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeButton() {
+    return (
+        <button type="button" className="btn btn-primary btn-sm">Home</button>
+    )
+};
+
+export default HomeButton;
