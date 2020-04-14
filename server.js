07693c0559f1cdefb29f6b0bd37ae27a73385c97
james davies <jamesdavies@jamess-imac.home>
2020-04-14 19:30:58 +0100

    added new model and route for the new user db and tested on postman. working successfully

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes/walks");
+const userRoutes = require("./routes/logIn");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,18 +13,18 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  });
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
 
-  const connection = mongoose.connection;
-  connection.once("open", () => {
-    console.log("MongoDB database connection established successfully!");
-  })
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
 
 app.use(routes);
+app.use(userRoutes);
 
 app.listen(PORT, () =>
   console.log(`API Server now listening on PORT ${PORT}!`)
